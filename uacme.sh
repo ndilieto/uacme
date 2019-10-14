ndilieto/uacme

commit 8f8b9faddcae58ba250923bfc2febf6229a6ef21

allow CHALLENGE_PATH to be exportable in an environment variable

uacme.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-CHALLENGE_PATH=/var/www/.well-known/acme-challenge
+CHALLENGE_PATH="${UACME_CHALLENGE_PATH:-'/var/www/.well-known/acme-challenge'}"
 ARGS=5
 E_BADARGS=85
 

commit 52861eab5de20af3d4a0838d7f44af5113a79f23

Fix default value of CHALLENGE_PATH variable in uacme.sh
Single quotes don't work inside parameter expansion as in other places.
They are not stripped during expansion but become part of the value.
Thus this will result in:

    CHALLENGE_PATH="'/var/www/.well-known/acme-challenge'"

Since the default path doesn't contain any spaces, it's not needed to
quote it at all.

This bug was introduced in 8f8b9faddcae58ba250923bfc2febf6229a6ef21.

uacme.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-CHALLENGE_PATH="${UACME_CHALLENGE_PATH:-'/var/www/.well-known/acme-challenge'}"
+CHALLENGE_PATH="${UACME_CHALLENGE_PATH:-/var/www/.well-known/acme-challenge}"
 ARGS=5
 E_BADARGS=85
 