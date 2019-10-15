7414a7d3ade7d6ff7497d652b8d5995feeb30e97
halosghost <samuel.stuewe@gmail.com>
2019-10-14 20:35:17 -0500

    remove errant single quotes from hook default challenge path

--- uacme.sh
+++ uacme.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-CHALLENGE_PATH="${UACME_CHALLENGE_PATH:-'/var/www/.well-known/acme-challenge'}"
+CHALLENGE_PATH="${UACME_CHALLENGE_PATH:-/var/www/.well-known/acme-challenge}"
 ARGS=5
 E_BADARGS=85
 
